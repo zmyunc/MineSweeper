6cf68ebbb9294514138f5e2bd4580f68f9e2dfa4
Ketan Mayer-Patel <kmp@cs.unc.edu>
2024-02-07 14:47:27 -0500

    Added bomb picking.

--- minesweeper_model.js
+++ minesweeper_model.js
@@ -7,7 +7,15 @@ let MineSweeperModel = function (width, height, bomb_count) {
     this.field = new MineSweeperField(this.width, this.height);
 
     // Randomly pick bomb_count cells within the field to have bombs.
+
+    let all_cells = [];
+    this.field.forAllCells(c => {
+            all_cells.push([c, Math.random()]);
+    });
     
+    all_cells.sort((a,b) => a[1]-b[1])
+         .slice(0,this.bomb_count)
+         .forEach(c => c[0].has_bomb = true);
 }
 
 let MineSweeperField = function(width, height) {
@@ -18,6 +26,14 @@ let MineSweeperField = function(width, height) {
             this.cells[x][y] = new MineSweeperCell();
         }
     }
+}
+
+MineSweeperField.forAllCells = function(fn) {
+    this.cells.forEach(column => {
+        column.forEach(cell => {
+            fn(cell);
+        });
+    });
 }
 
 let MineSweeperCell = function() {
